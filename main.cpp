5b89994ba50bb5a3866cd3e56e2b6a1f2d90e786
Adham Zahran <adham.zahran@hotmail.com>
2015-12-08 04:23:04 +0200

    training project template with documentation

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv[])
+{
+  // This is a calculator
+  // It is supposed to take commands on the form of
+  // awesomecalculator add 3 5
+  // and the output would be 8
+  // so argv[1] is the operation. ex. add, subtract, multiply, divide, power, root, log and factorial.
+  // argv[2] and argv[3] are the operands.
+  // each projects member will implement one operation function.
+  // reda will implement the main body.
+  return 0;
+}
