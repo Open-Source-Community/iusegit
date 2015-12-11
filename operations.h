fbc0339b23d89516b93e41b88de3c0e7e5044398
yasminGhazy <jesygzaki@gmail.com>
2015-12-11 20:06:48 +0200

    add func

--- operations.h
+++ operations.h
@@ -3,4 +3,11 @@ float subtract(float num1, float num2)
 	float result = num1-num2 ;
 	return result;
 }
-
+double log (double x)
+{
+    cout<<"log base 10 ="<<log10(x)<<endl;
+}
+double log2(double x )
+{
+    cout<<"log base "<<log2(x);
+}
