a9808898fdcbdcd0125516c290c4cc7f09d791e5
Rana Hussam <rana_ranuna@live.com>
2016-02-08 01:45:11 +0200

    edit add function

--- functions.cpp
+++ functions.cpp
@@ -1,4 +1,10 @@
-float add(float num1, float num2);
+float add(float num1, float num2)
+{
+    float sum=0;
+    sum = num1+num2;
+    return sum;
+}
+
 float subtract(float num1, float num2);
 float multiply(float num1, float num2);
 float divide(float num1, float num2);
